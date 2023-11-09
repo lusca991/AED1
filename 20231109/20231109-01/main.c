d1a44c746e994cb09734f31c9d0f8de5b0b355f8
lusca991 <lbv.lic20@uea.edu.br>
2023-11-09 11:50:18 -0400

    alteração no main

--- 20231109/20231109-01/main.c
+++ 20231109/20231109-01/main.c
@@ -5,19 +5,19 @@ int main() {
     int vetor[] = {10, 5, 8, 2, 14, 7, 3};
     int tamanho = sizeof(vetor) / sizeof(vetor[0]);
 
-    // Encontrar o elemento máximo no vetor
+    //encontrar o elemento máximo no vetor
     int maximo = encontraMaximo(vetor, tamanho);
     printf("Elemento máximo: %d\n", maximo);
 
-    // Encontrar o elemento mínimo no vetor
+    //encontrar o elemento mínimo no vetor
     int minimo = encontraMinimo(vetor, tamanho);
     printf("Elemento mínimo: %d\n", minimo);
 
-    // Calcular a soma dos elementos no vetor
+    //calcular a soma dos elementos no vetor
     int soma = calculaSoma(vetor, tamanho);
     printf("Soma dos elementos: %d\n", soma);
 
-    // Calcular o produto dos elementos no vetor
+    //calcular o produto dos elementos no vetor
     int produto = calculaProduto(vetor, tamanho);
     printf("Produto dos elementos: %d\n", produto);
 
